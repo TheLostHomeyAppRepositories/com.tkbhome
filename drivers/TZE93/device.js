329c33b2f82869a5da5bad6bc8b8a57bba324202
pshotton <phil.shotton@cloudscapesolutions.com>
2023-12-03 10:18:56 +0000

    3.0.6 - Added TZE93 Thermostat

---
+++ drivers/TZE93/device.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Homey = require('homey');
+const ZwaveDevice = require('homey-zwavedriver').ZwaveDevice;
+
+const { ManagerZwave } = require('homey');
+
+class TZE93 extends ZwaveDevice {
+
+	onNodeInit() {
+
+		//enable debugging
+		this.enableDebug();
+
+		//print the node's info to the console
+		this.printNode();
+
+		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
+			getOpts: {
+				getOnStart: true,
+				// pollInterval: 'poll_interval_TEMPERATURE',
+				// pollMultiplication: 60000,
+			},
+		});
+		this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT', {
+			getOpts: {
+				getOnStart: true,
+			},
+		});
+	}
+
+}
+
+module.exports = TZE93;
