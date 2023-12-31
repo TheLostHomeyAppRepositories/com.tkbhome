90935125242981b71d7a49535fd37e93d0e89ac2
pshotton <phil.shotton@cloudscapesolutions.com>
2023-12-31 20:50:36 +0000

    3.0.7 - Added onoff to driver...

--- drivers/TZE93/device.js
+++ drivers/TZE93/device.js
@@ -15,6 +15,8 @@ class TZE93 extends ZwaveDevice {
 		//print the node's info to the console
 		this.printNode();
 
+		this.registerCapability('onoff', 'SWITCH_MULTILEVEL');
+
 		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
 			getOpts: {
 				getOnStart: true,
